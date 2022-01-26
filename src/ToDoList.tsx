fd5e333ae2ab2e83f1ab7eb549960c74b009db04
codeer <loginonlyyy@gmail.com>
2022-01-26 23:02:57 +0900

    to-do list set up

---
+++ src/ToDoList.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  const [toDo, setToDo] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(toDo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Write a to-do"
+          value={toDo}
+          onChange={onChange}
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoList;
