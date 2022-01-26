6e69984ca9a976135946c2b39ec220f68ee9ad0a
codeer <loginonlyyy@gmail.com>
2022-01-27 00:03:17 +0900

    form validation using react-hook-form

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -2,20 +2,37 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, watch, handleSubmit, formState } = useForm();
+  console.log(formState.errors);
+  const onValid = (data: any) => {
+    console.log(data);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit(onValid)}>
         <input
           type="text"
           {...register("firstName")}
           placeholder="First Name"
         />
-        <input type="text" {...register("lastName")} placeholder="Last Name" />
+        <input
+          type="text"
+          {...register("lastName", { required: true, minLength: 10 })}
+          placeholder="Last Name"
+        />
         <input type="text" {...register("email")} placeholder="email" />
         <input type="text" {...register("username")} placeholder="username" />
-        <input type="text" {...register("password")} placeholder="Password" />
+        <input
+          type="text"
+          {...register("password", {
+            required: "Password is needed",
+            minLength: {
+              value: 10,
+              message: "Password should be longer than 10 letters",
+            },
+          })}
+          placeholder="Password"
+        />
         <input
           type="text"
           {...register("passwordConfirmation")}
